e12df6ac79bf440094f773f3b7225a2d8536298b
Wenhao Li <117025921+peacelwh@users.noreply.github.com>
2025-05-19 21:45:26 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,148 @@
-# VT-FSL
-we propose a novel framework, bridging Vision and Text with LLMs for Few-Shot Learning (VT-FSL), which constructs precise cross-modal prompts conditioned on LLMs and support images, seamlessly integrating them through a geometry-aware alignment mechanism.
+# VT-FSL: Bridging Vision and Text with LLMs for Few-Shot Learning
+
+
+# Method
+
+We propose a novel framework, bridging Vision and Text with LLMs for Few-Shot Learning (VT-FSL), which constructs precise cross-modal prompts conditioned on Large Language Models (LLMs) and support images, seamlessly integrating them through a geometry-aware alignment mechanism.
+
+1. **VT-FSL Intuition**
+
+<img src='img/fig1.png'>
+
+2. **VT-FSL Overview**
+
+<img src='img/fig2.png'>
+
+## Standard Few-Shot Classification Results
+|  Dataset  | 1-Shot 5-Way | 5-Shot 5-Way |  
+|:--------:|:------------:|:------------:|
+| FC100 |    57.99 ± 0.40     |    67.68 ± 0.38    |
+|  CIFAR-FS  |     88.67 ± 0.33     |     91.45 ± 0.46    |
+| MiniImageNet |    83.66 ± 0.31     |     88.38 ± 0.25    |
+| TieredImageNet |      88.02 ± 0.34     |     91.97 ± 0.27    |
+
+## Fine-grained Few-Shot Classification Results
+|  Dataset  | 1-Shot 5-Way | 5-Shot 5-Way |  
+|:--------:|:------------:|:------------:|
+| CUB |    91.08 ± 0.28     |    94.63 ± 0.19    |
+|  Dogs  |     86.58 ± 0.30     |     90.69 ± 0.25     |
+| Cars |    92.95 ± 0.24     |     96.62 ± 0.15    |
+
+## Cross Domain Few-shot Classification Results
+|  Dataset  | 1-Shot 5-Way | 5-Shot 5-Way |  
+|:--------:|:------------:|:------------:|
+| CUB      |    66.86 ± 0.47     |    81.02 ± 0.36     |
+|  Places  |    73.68 ± 0.41     |    81.52 ± 0.33    |
+|  Plantae |    45.90 ± 0.40     |    61.54 ± 0.38    |
+
+
+# Requirements
+
+- [PyTorch and torchvision](https://pytorch.org)
+
+- Install packages:
+```python
+pip instal -r requirements.txt
+```
+
+
+# Datasets
+
+- Dataset: [Google Cloud](https://drive.google.com/drive/folders/1xmXqS5AZAJpifyg2uyTnTXgWXeK9HthG?usp=drive_link) 
+- Please download the dataset you need and then put the xxx.tar.gz in ./dataset directory:
+```python
+cd ./dataset
+tar -xvzf xxx.tar.gz
+```
+
+
+# Reproducing Results with Pretrained Checkpoints
+To directly reproduce the results reported in the paper using our trained models:
+
+1. Download Weights
+
+- Pre-training and Meta-tuning weights: [Google Cloud](https://drive.google.com/drive/folders/1qvXTPBmiw3Rb4Aj2uopvYKri4fm7-X2I?usp=sharing)
+- Please download checkpoints and then put them into ./checkpoints directory.
+
+2. Run Inference
+Run the evaluation script with the desired settings. For example, to evaluate on the miniImageNet dataset with a 5-way 5-shot configuration:
+```python
+python method/test.py \
+    --dataset miniImageNet \
+    --way 5 \
+    --shot 5 \
+    --episode 2000 \
+    --image_size 224 \
+    --gpu 0
+
+```
+This will evaluate the pretrained model on 2000 few-shot episodes using the specified configuration. 
+
+3. Expected Results
+You should observe performance consistent with the results reported in our paper. If results slightly vary, it may be due to sampling randomness; we recommend running with a fixed seed or averaging over multiple runs.
+
+
+
+# Training from Scratch
+If you prefer to train the model from scratch instead of using the provided pretrained weights, follow the two-stage training process described below. We provide example scripts for the miniImageNet dataset.
+
+1. Pre-train the Feature Extractor
+Run the following command to pre-train the visual backbone on the base split of miniImageNet:
+```python
+python method/pretrain.py \
+    --dataset miniImageNet \
+    --batch_size 512 \
+    --image_size 224 \
+    --backbone visformer-t \
+    --lr 5e-4 \
+    --epoch 800 \
+    --gpu 0
+
+```
+
+2. Meta-tune with VT-FSL
+After pretraining, meta-tune the model for few-shot learning using the episodic training strategy.
+- For 5-way 1-shot setting:
+```python
+python method/train.py \
+    --dataset miniImageNet \
+    --way 5 \
+    --shot 1 \
+    --image_size 224 \
+    --backbone visformer-t \
+    --lr 5e-4 \
+    --epoch 100 \
+    --t 0.2 \
+    --gpu 0
+
+```
+
+- For 5-way 5-shot setting:
+```python
+python method/train.py \
+    --dataset miniImageNet \
+    --way 5 \
+    --shot 5 \
+    --image_size 224 \
+    --backbone visformer-t \
+    --lr 5e-4 \
+    --epoch 100 \
+    --t 0.2 \
+    --gpu 0
+
+```
+After training, the model checkpoints will be automatically saved for evaluation.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,280 @@
+import math
+import os
+import random
+import warnings
+import time
+
+import numpy as np
+import torch
+from sklearn.cluster import KMeans
+from sklearn.linear_model import LogisticRegression
+import torch.nn.functional as F
+from torchvision import transforms
+from torch.utils.data import Subset, DataLoader
+from collections import defaultdict
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)  # CPU
+    torch.cuda.manual_seed(seed)  # GPU
+    torch.cuda.manual_seed_all(seed)  # All GPU
+    os.environ['PYTHONHASHSEED'] = str(seed)  # 禁止hash随机化
+    torch.backends.cudnn.deterministic = True  # 确保每次返回的卷积算法是确定的
+    torch.backends.cudnn.benchmark = False
+
+transform_train = transforms.Compose([
+    transforms.Resize(84),
+    transforms.CenterCrop(84),
+    transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(np.array([x / 255.0 for x in [120.39586422, 115.59361427, 104.54012653]]),
+                         np.array([x / 255.0 for x in [70.68188272, 68.27635443, 72.54505529]]))
+])
+
+transform_val = transforms.Compose([
+    transforms.Resize(84),
+    transforms.CenterCrop(84),
+    transforms.ToTensor(),
+    transforms.Normalize(np.array([x / 255.0 for x in [120.39586422, 115.59361427, 104.54012653]]),
+                         np.array([x / 255.0 for x in [70.68188272, 68.27635443, 72.54505529]]))
+])
+
+resize = 224
+transform_train_224_cifar = transforms.Compose([
+    transforms.Resize((resize, resize)),
+    transforms.CenterCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4),
+    transforms.ToTensor(),
+    # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))  # ImageNet standard
+    transforms.Normalize((0.5071, 0.4866, 0.4409), (0.2009, 0.1984, 0.2023))  # Differs from ImageNet standard!
+])
+
+transform_val_224_cifar = transforms.Compose([
+    transforms.Resize((resize, resize)),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))  # ImageNet standard
+    transforms.Normalize((0.5071, 0.4866, 0.4409), (0.2009, 0.1984, 0.2023))  # Differs from ImageNet standard!
+])
+
+transform_train_224 = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.CenterCrop(224),
+    # transforms.RandomHorizontalFlip(),
+    # transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4),
+    transforms.ToTensor(),
+    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))  # ImageNet standard
+    # transforms.Normalize((0.5071, 0.4866, 0.4409), (0.2009, 0.1984, 0.2023))  # Differs from ImageNet standard!
+])
+
+transform_val_224 = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))  # ImageNet standard
+    # transforms.Normalize((0.5071, 0.4866, 0.4409), (0.2009, 0.1984, 0.2023))  # Differs from ImageNet standard!
+])
+
+Normalize = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+
+transform_train_cifar = transforms.Compose([
+    # transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    Normalize
+])
+
+transform_val_cifar = transforms.Compose([
+    transforms.ToTensor(),
+    Normalize
+])
+
+#FG dataset
+mean = ([x / 255.0 for x in [125.3, 123.0, 113.9]])
+std = ([x / 255.0 for x in [63.0, 62.1, 66.7]])
+norm_params = {"mean": mean, 
+            "std": std}
+norm = transforms.Normalize(**norm_params)
+
+transform_train_fg = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    norm  
+])
+
+transform_val_fg = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    norm
+])
+
+def compute_n_params(model, return_str=True):
+    tot = 0
+    for p in model.parameters():
+        w = 1
+        for x in p.shape:
+            w *= x
+        tot += w
+    if return_str:
+        if tot >= 1e6:
+            return '{:.1f}M'.format(tot / 1e6)
+        else:
+            return '{:.1f}K'.format(tot / 1e3)
+    else:
+        return tot
+
+
+def compute_acc(logits, label, reduction='mean'):
+    ret = (torch.argmax(logits, dim=1) == label).float()
+    if reduction == 'none':
+        return ret.detach()
+    elif reduction == 'mean':
+        return ret.mean().item()
+
+def compute_acc_mix(logits, label, reduction='mean'):
+    if len(label.shape) > 1:
+        label = torch.argmax(label, dim=1)
+    
+    correct_predictions = (torch.argmax(logits, dim=1) == label).float()
+    
+    if reduction == 'mean':
+        return correct_predictions.mean().item()
+    elif reduction == 'sum':
+        return correct_predictions.sum().item()
+    else:
+        return correct_predictions
+
+def count_95acc(accuracies):
+    acc_avg = np.mean(np.array(accuracies))
+    acc_ci95 = 1.96 * np.std(np.array(accuracies)) / np.sqrt(len(accuracies))
+    return acc_avg, acc_ci95
+
+def ensure_path(path, remove=True):
+    basename = os.path.basename(path.rstrip('/'))
+    if os.path.exists(path):
+        raise FileExistsError(f"Error: The path '{path}' already exists. Please provide a different path.")
+    else:
+        os.makedirs(path)
+
+class Averager():
+
+    def __init__(self):
+        self.n = 0.0
+        self.v = 0.0
+
+    def add(self, v, n=1.0):
+        self.v = (self.v * self.n + v * n) / (self.n + n)
+        self.n += n
+
+    def item(self):
+        return self.v
+    
+
+
+def set_log_path(path):
+    global _log_path
+    _log_path = path
+
+def log(obj, filename='train'):
+    filename += '.txt'
+    print(obj)
+    if _log_path is not None:
+        with open(os.path.join(_log_path, filename), 'a') as f:
+            print(obj, file=f)
+
+class Timer():
+
+    def __init__(self):
+        self.v = time.time()
+
+    def s(self):
+        self.v = time.time()
+
+    def t(self):
+        return time.time() - self.v
+
+def time_str(t):
+    if t >= 3600:
+        return '{:.1f}h'.format(t / 3600)
+    if t >= 60:
+        return '{:.1f}m'.format(t / 60)
+    return '{:.1f}s'.format(t)
+
+def cluster(data, n_clusters=64, num=600):
+    x = []
+    label = []
+    for k, v in data.items():
+        x.extend(v)
+        label.append(k)
+    data = 0
+    y = np.arange(len(label)).repeat(len(v))
+    x = np.array(x)
+    kmeans = KMeans(n_clusters=n_clusters)
+    kmeans.fit(x, y)
+    k_center = kmeans.cluster_centers_
+    k_label = kmeans.labels_
+    center = {}
+    for k in range(len(label)):
+        labels = k_label[k * num:(k + 1) * num]
+        counts = np.bincount(labels)
+        index = np.argmax(counts)
+        center[label[k]] = k_center[index]
+    return center
+
+
+class MultiTrans:
+    def __init__(self, trans):
+        self.trans = trans
+
+    def __call__(self, x):
+        out = []
+        for trans in self.trans:
+            out.append(trans(x))
+        return out
+
+transform = transforms.Compose([
+                    transforms.Resize(int(224 * 1.1)),
+                    transforms.CenterCrop(224),
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+                ])
+
+aug = transforms.Compose([
+    transforms.RandomResizedCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+])
+transform = MultiTrans([transform]*3 + [aug]*(10-3))
+
+
+def convert_raw(dataset,x):
+    if dataset == 'miniImageNet':
+        norm_params = {"mean": [0.485, 0.456, 0.406],
+                        "std": [0.229, 0.224, 0.225]}
+    else:
+        norm_params = {"mean": [0.5071, 0.4866, 0.4409],
+                        "std": [0.2009, 0.1984, 0.2023]}
+
+    mean = torch.tensor(norm_params['mean']).view(3, 1, 1).type_as(x)
+    std = torch.tensor(norm_params['std']).view(3, 1, 1).type_as(x)
+    return x * std + mean
+
+def visualize_dataset(dataset, name, writer, n_samples=16, step=0):
+    demo = []
+    for i in np.random.choice(len(dataset), n_samples):
+        demo.append(convert_raw(dataset,dataset[i][0]))
+    writer.add_images('visualize_' + name, torch.stack(demo), global_step=step)
+    writer.flush()
+
+transform_cam = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))  # ImageNet standard
+    # transforms.Normalize((0.5071, 0.4866, 0.4409), (0.2009, 0.1984, 0.2023))  # Differs from ImageNet standard!
+])
